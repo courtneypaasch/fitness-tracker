9a5f52e424f9b107d30537ce4201418b0e61caaa
Courtney Paasch <paasch.courtney@gmail.com>
2020-11-21 13:42:42 -0500

    add user model

---
+++ models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date
+    },
+    exercises: [{
+        type: {
+            type: String,
+        },
+        name: {
+            type: String,
+        },
+        duration: {
+            type: Number,
+        },
+        weight: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        distance: {
+            type: Number,
+        },
+    }],
+}, {
+    toJSON: {
+        virtuals: true
+    }
+});
+WorkoutSchema.virtual('totalDuration').get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+const Workout = mongoose.model('Workout', WorkoutSchema);
+
+module.exports = Workout;
